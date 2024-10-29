e77bc4cd70ac43d77568beb04a4f89a35757264f
Stanislav Valášek <valasek@gmail.com>
2024-10-29 15:52:00 +0000

    Kamal proxy for www and pure findacoach.eu

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,8 +97,8 @@ Rails.application.configure do
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
+  #   "findacoach.eu",     # Allow requests from findacoach.eu
+  #   /.*\.findacoach\.eu/ # Allow requests from subdomains like `www.findacoach.eu`
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
