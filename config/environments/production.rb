63073adc7085a3582028a9dbf3d80f6ce25a6323
Stanislav Valášek <valasek@gmail.com>
2024-10-29 16:05:49 +0000

    Kamal proxy for www and pure findacoach.eu

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,5 +101,5 @@ Rails.application.configure do
   #   /.*\.findacoach\.eu/ # Allow requests from subdomains like `www.findacoach.eu`
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
-#   config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-# end
+  # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+end
