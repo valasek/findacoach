da24fd1da480acc9c5d88a2e80290db5bc91fdee
Stanislav Valášek <valasek@gmail.com>
2024-10-31 14:19:10 +0000

    Dashboard numbers fixed

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
   def index
     @clients_count = current_user.clients.count
     @hours_count = current_user.total_coaching_hours
-    @coaching_sessions_paid = current_user.percentage_paid_sessions
-    @coaching_sessions_count = current_user.sessions.count
+    @sessions_paid = current_user.percentage_paid_sessions
+    @sessions_count = current_user.sessions.count
   end
 end
--- app/controllers/whats_new_controller.rb
+++ app/controllers/whats_new_controller.rb
@@ -2,6 +2,23 @@ class WhatsNewController < ApplicationController
   def index
     @changelog_entries = [
       {
+        version: "0.2",
+        date: "October 31, 2024",
+        changes: {
+          "New" => [
+            "Export to MS Excel, compliant with ICF requirements",
+            "Security section on home page"
+          ],
+          "Changed" => [
+            "Added statistics to the dashboard"
+          ]
+          # "Fixed" => [
+          #   "",
+          #   ""
+          # ]
+        }
+      },
+      {
         version: "0.1",
         date: "October 23, 2024",
         changes: {
@@ -20,7 +37,6 @@ class WhatsNewController < ApplicationController
           # ]
         }
       }
-      # Add more changelog entries here
     ]
   end
 end
