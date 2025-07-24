4e96e9b3c7df0dc6e3ffe5ba822f956f5316e59d
Stanislav Valášek <valasek@gmail.com>
2025-07-24 06:49:03 +0000

    session ordering

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,7 +1,7 @@
 class Session < ApplicationRecord
   belongs_to :client
 
-  default_scope { order(:date) }
+  default_scope { order(date: :desc) }
 
   validates :date, presence: true
   validates :duration, presence: true, numericality: { greater_than_or_equal_to: 0.0, less_than_or_equal_to: 12.0, step: 0.5 }
