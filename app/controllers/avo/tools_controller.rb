037975861bce0bee33a8fb5d68d893c5f5556172
Stanislav Valasek <valasek@gmail.com>
2026-05-16 14:06:17 +0200

    Dashboard sorted by # of clients

--- app/controllers/avo/tools_controller.rb
+++ app/controllers/avo/tools_controller.rb
@@ -2,6 +2,6 @@ class Avo::ToolsController < Avo::ApplicationController
   def dashboard
     @page_title = "Dashboard"
     add_breadcrumb "Dashboard"
-    @users = User.all
+    @users = User.left_joins(:clients).group(:id).order("COUNT(clients.id) DESC")
   end
 end
